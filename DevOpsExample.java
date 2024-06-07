7d00046ddbeeb1e7b040ceb76e2320675cc74c15
subha4521er <168360241+subha4521er@users.noreply.github.com>
2024-06-07 22:12:10 +0530

    Update DevOpsExample.java

---
+++ DevOpsExample.java
@@ -0,0 +1,9 @@
+
+/**
+ * The HelloWorldApp class implements an appication that simply prints "Hello World" to standard output.
+ */
+public class DevOpsExample {
+  public static void main(strin[] args) {
+    System.out.println("Welcome to DevOps.");
+  }
+}
